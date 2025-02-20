c574b209e40b1dde108d43afe36c947328e01503
Ron McLaren <ronlaren@aol.com>
2025-02-20 09:57:35 -0500

    initial checkin for model_data code

---
+++ model_data/mapping/bufr_atms_mapping.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+import sys
+sys.path.insert(0, '../src')
+
+import os
+import argparse
+import time
+import numpy as np
+import bufr
+from wxflow import Logger
+
+from zarr_encoder import zarrEncoder
+
+# Initialize Logger
+# Get log level from the environment variable, default to 'INFO it not set
+log_level = os.getenv('LOG_LEVEL', 'INFO')
+logger = Logger('bufr_atms.py', level=log_level, colored_log=False)
+
+
+def logging(comm, level, message):
+    """
+  Logs a message to the console or log file, based on the specified logging level.
+
+  This function ensures that logging is only performed by the root process (`rank 0`)
+  in a distributed computing environment. The function maps the logging level to
+  appropriate logger methods and defaults to the 'INFO' level if an invalid level is provided.
+
+  Parameters:
+      comm: object
+          The communicator object, typically from a distributed computing framework
+          (e.g., MPI). It must have a `rank()` method to determine the process rank.
+      level: str
+          The logging level as a string. Supported levels are:
+              - 'DEBUG'
+              - 'INFO'
+              - 'WARNING'
+              - 'ERROR'
+              - 'CRITICAL'
+          If an invalid level is provided, a warning will be logged, and the level
+          will default to 'INFO'.
+      message: str
+          The message to be logged.
+
+  Behavior:
+      - Logs messages only on the root process (`comm.rank() == 0`).
+      - Maps the provided logging level to a method of the logger object.
+      - Defaults to 'INFO' and logs a warning if an invalid logging level is given.
+      - Supports standard logging levels for granular control over log verbosity.
+
+  Example:
+      >>> logging(comm, 'DEBUG', 'This is a debug message.')
+      >>> logging(comm, 'ERROR', 'An error occurred!')
+
+  Notes:
+      - Ensure that a global `logger` object is configured before using this function.
+      - The `comm` object should conform to MPI-like conventions (e.g., `rank()` method).
+  """
+
+    if comm.rank() == 0:
+        # Define a dictionary to map levels to logger methods
+        log_methods = {
+            'DEBUG': logger.debug,
+            'INFO': logger.info,
+            'WARNING': logger.warning,
+            'ERROR': logger.error,
+            'CRITICAL': logger.critical,
+        }
+
+        # Get the appropriate logging method, default to 'INFO'
+        log_method = log_methods.get(level.upper(), logger.info)
+
+        if log_method == logger.info and level.upper() not in log_methods:
+            # Log a warning if the level is invalid
+            logger.warning(f'log level = {level}: not a valid level --> set to INFO')
+
+        # Call the logging method
+        log_method(message)
+
+
+def _make_description(mapping_path, update=False):
+    description = bufr.encoders.Description(mapping_path)
+
+    if update:
+        # Define the variables to be added in a list of dictionaries
+        variables = [
+        ]
+
+        # Loop through each variable and add it to the description
+        for var in variables:
+            description.add_variable(
+                name=var['name'],
+                source=var['source'],
+                units=var['units'],
+                longName=var['longName']
+            )
+
+    return description
+
+
+def _make_obs(comm, input_path, mapping_path):
+    # Get container from mapping file first
+    container = bufr.Parser(input_path, mapping_path).parse(comm)
+
+
+    return container
+
+
+def create_obs_file(input_path, mapping_path, output_path):
+    comm = bufr.mpi.Comm("world")
+    container = _make_obs(comm, input_path, mapping_path)
+    container.gather(comm)
+
+    description = _make_description(mapping_path, update=True)
+
+    # Encode the data
+    if comm.rank() == 0:
+        zarrEncoder(description).encode(container, output_path, append=True)
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    bufr.mpi.App(sys.argv)
+    comm = bufr.mpi.Comm("world")
+
+    # Required input arguments as positional arguments
+    parser = argparse.ArgumentParser(description="Convert BUFR to NetCDF using a mapping file.")
+    parser.add_argument('input', type=str, help='Input BUFR file')
+    parser.add_argument('mapping', type=str, help='BUFR2IODA Mapping File')
+    parser.add_argument('output', type=str, help='Output NetCDF file')
+
+    args = parser.parse_args()
+    infile = args.input
+    mapping = args.mapping
+    output = args.output
+
+    create_obs_file(infile, mapping, output)
+
+    end_time = time.time()
+    running_time = end_time - start_time
+    logging(comm, 'INFO', f'Total running time: {running_time}')
---
+++ model_data/src/zarr_encoder.py
@@ -0,0 +1,178 @@
+
+import os
+import re
+from typing import Union
+
+import zarr
+import numpy as np
+
+import bufr
+
+DEFAULT_CHUNK_SIZE = 5000
+
+
+# Encoder for Zarr format
+class Encoder(bufr.encoders.EncoderBase):
+    def __init__(self, description: Union[str, bufr.encoders.Description]):
+        if isinstance(description, str):
+            self.description = bufr.encoders.Description(description)
+        else:
+            self.description = description
+
+        super(Encoder, self).__init__(self.description)
+
+    def encode(self,
+               container:bufr.DataContainer,
+               output_template_path:str,
+               append=False) -> dict[tuple[str],zarr.Group]:
+
+        result:dict[tuple[str], zarr.Group] = {}
+        for category in container.all_sub_categories():
+            cat_idx = 0
+            substitutions = {}
+            for key in container.get_category_map().keys():
+                substitutions[key] = category[cat_idx]
+                cat_idx += 1
+
+            output_path = self._make_path(output_template_path, substitutions)
+
+            store = zarr.DirectoryStore(output_path)
+            root = zarr.group(store=store, overwrite=(not append))
+
+            if 'time' not in root:
+                self._add_attrs(root)
+
+                dims = self.get_encoder_dimensions(container, category)
+                self._init_dimensions(root, container, category, dims)
+                self._init_variables(root, container, category, dims)
+            else:
+                self._append_data(root, container, category)
+
+            # Close the zarr file
+            root.store.close()
+
+            result[tuple(category)] = root
+
+        return result
+
+    # def _init_zarr_file(self, output_path):
+
+
+    def _add_attrs(self, root:zarr.Group):
+        # Adds globals as attributes to the root group
+        for key, data in self.description.get_globals().items():
+            root.attrs[key] = data
+
+    def _init_dimensions(self,
+                         root:zarr.Group,
+                         container: bufr.DataContainer,
+                         category:[str],
+                         dims:bufr.encoders.EncoderDimensions):
+
+        # Add the time dimension as the primary dimension
+        timestamps = container.get('variables/timestamp', category)
+        dim_store = root.create_dataset('time',
+                                        shape=[len(timestamps)],
+                                        chunks=[DEFAULT_CHUNK_SIZE],
+                                        dtype=timestamps.dtype,
+                                        compression='blosc',
+                                        compression_opts=dict(cname='lz4',
+                                                              clevel=3,
+                                                              shuffle=1))
+        dim_store[:] = timestamps
+        dim_store.attrs['_ARRAY_DIMENSIONS'] = ['time']
+
+        # Add the backing variables for the dimensions
+        for dim in dims.dims():
+            # Skip the location dimension as we will use the timestamp as the location
+            if dim.name == 'Location':
+                continue
+
+            dim_data = dim.labels
+            dim_store = root.create_dataset(dim.name().lower(),
+                                            shape=[len(dim_data)],
+                                            chunks=[DEFAULT_CHUNK_SIZE],
+                                            dtype=np.int32,
+                                            compression='blosc',
+                                            compression_opts=dict(cname='lz4',
+                                                                  clevel=3,
+                                                                  shuffle=1),
+                                            maxshape=(DEFAULT_CHUNK_SIZE))
+            dim_store[:] = dim_data
+            dim_store.attrs['_ARRAY_DIMENSIONS'] = [dim.name().lower()]
+
+    def _init_variables(self,
+                        root:zarr.Group,
+                        container: bufr.DataContainer,
+                        category:[str],
+                        dims:bufr.encoders.EncoderDimensions):
+
+        for var in self.description.get_variables():
+            _, var_name = self._split_source_str(var['name'])
+
+            if var_name == 'dateTime':
+                continue # Skip the time variable as it is a dimension
+
+            if var["source"] not in container.list():
+                raise ValueError(f'Variable {var["source"]} not found in the container')
+
+            comp_level = var['compressionLevel'] if 'compressionLevel' in var else 3
+            var_data = container.get(var['source'], category)
+
+            # Create the zarr dataset
+            store = root.create_dataset(var_name,
+                                        shape=var_data.shape,
+                                        # chunks=dims.chunks_for_var(var['name']),
+                                        chunks=(DEFAULT_CHUNK_SIZE),
+                                        dtype=var_data.dtype,
+                                        compression='blosc',
+                                        compression_opts=dict(cname='lz4',
+                                                              clevel=comp_level,
+                                                              shuffle=1))
+            store[:] = var_data
+
+            # Add the attributes
+            store.attrs['units'] = var['units']
+            store.attrs['long_name'] = var['longName']
+
+            if 'coordinates' in var:
+                store.attrs['coordinates'] = var['coordinates']
+
+            if 'range' in var:
+                store.attrs['valid_range'] = var['range']
+
+
+            # Associate the dimensions
+            dim_names = dims.dim_names_for_var(var["name"])
+            dim_names = [dim_name.lower() for dim_name in dim_names]
+            dim_names[0] = 'time'
+
+            store.attrs['_ARRAY_DIMENSIONS'] = dim_names
+
+    def _append_data(self,
+                     root:zarr.Group,
+                     container: bufr.DataContainer,
+                     category:[str]):
+
+        for var in self.description.get_variables():
+            _, var_name = self._split_source_str(var['name'])
+
+            if var_name == 'dateTime':
+                var_name = 'time'
+
+            var_data = container.get(var['source'], category)
+            root[var_name].append(var_data)
+
+    def _make_path(self, prototype_path:str, sub_dict:dict[str, str]):
+        subs = re.findall(r'\{(?P<sub>\w+\/\w+)\}', prototype_path)
+        for sub in subs:
+            prototype_path = prototype_path.replace(f'{{{sub}}}', sub_dict[sub])
+
+        return prototype_path
+
+    def _split_source_str(self, source:str) -> (str, str):
+        components = source.split('/')
+        group_name = components[0]
+        variable_name = components[1]
+
+        return (group_name, variable_name)
