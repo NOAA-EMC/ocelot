67e4c5bcbbc3ad93ba9a5f56e24de2cd5162b32f
Ron McLaren <ronlaren@aol.com>
2025-03-01 16:20:11 -0500

    add configure script

---
+++ model_data/configs/configure.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import pathlib
+import shutil
+
+configs_path = os.path.join(pathlib.Path(__file__).parent.resolve(), './')
+settings_path = os.path.join(configs_path, 'settings.py')
+
+if not os.path.exists(settings_path):
+    template_path = os.path.join(configs_path, 'template_settings.py')
+    shutil.copyfile(template_path, settings_path)
+
+os.system(f"vi {settings_path}")
+
