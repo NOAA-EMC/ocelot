f49020e4d442621522c8cf7254c451a1a5179061
Ron McLaren <ronlaren@aol.com>
2025-03-02 14:20:28 -0500

    added normalize_atms script

--- model_data/scripts/normalize_atms.py
+++ model_data/scripts/normalize_atms.py
@@ -1,4 +1,5 @@
 import os
+import re
 import argparse
 import zarr
 from sklearn.preprocessing import StandardScaler
@@ -8,22 +9,29 @@ def normalize(data):
     scaler.fit(data)
     return scaler.transform(data)
 
+def apply_normalization(zarr_file):
+    with zarr.open(args.output, mode='a') as zarr_file:
+        fields_names = ['sensorZenithAngle', 'solarAzimuthAngle']
+
+        # add the bt_channel fields
+        for key in zarr_file.keys():
+            if key.startswith('bt_channel'):
+                fields_names.append(key)
+
+        for field in fields_names:
+            data = zarr_file[field][:]
+            zarr_file[field][:] = normalize(data)
+
+        zarr_file.save()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='Input zarr file')
-    parser.add_argument('output', type=str, help='Output zarr file')
     args = parser.parse_args()
 
-    fields_names = ['sensorZenithAngle', 'solarAzimuthAngle', 'bt_channel_1']
+    output = re.sub(r'\.zarr$', '_normalized.zarr', args.input)
 
     # copy input zarr file to output zarr file
-    os.system(f'cp {args.input} {args.output}')
-
-    # open zarr file and apply normalization to the fields, then save the modified data the output zarr file
-    zarr_file = zarr.open(args.output, mode='a')
-    for field in fields_names:
-        data = zarr_file[field][:]
-        zarr_file[field][:] = normalize(data[:])
-
-    zarr_file.save()
-    zarr_file.close()
+    os.system(f'cp {args.input} {output}')
+    apply_normalization(output)
