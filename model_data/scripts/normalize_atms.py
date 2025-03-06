33d807354e43060d22350dc2d697f52ff5ecd6f5
Ronald McLaren <ronald.mclaren@noaa.gov>
2025-03-06 20:23:11 +0000

    fixed things

--- model_data/scripts/normalize_atms.py
+++ model_data/scripts/normalize_atms.py
@@ -9,20 +9,21 @@ def normalize(data):
     scaler.fit(data)
     return scaler.transform(data)
 
-def apply_normalization(zarr_file):
-    with zarr.open(args.output, mode='a') as zarr_file:
-        fields_names = ['sensorZenithAngle', 'solarAzimuthAngle']
+def apply_normalization(output):
+    zarr_file = zarr.open(output, mode='a')
+    fields_names = ['sensorZenithAngle', 'solarAzimuthAngle']
 
-        # add the bt_channel fields
-        for key in zarr_file.keys():
-            if key.startswith('bt_channel'):
-                fields_names.append(key)
+    # add the bt_channel fields
+    for key in zarr_file.keys():
+        if key.startswith('bt_channel'):
+            fields_names.append(key)
 
-        for field in fields_names:
-            data = zarr_file[field][:]
-            zarr_file[field][:] = normalize(data)
+    for field in fields_names:
+        data = zarr_file[field][:]
+        zarr_file[field][:] = normalize(data)
 
-        zarr_file.save()
+    zarr_file.save()
+    zarr_file.close()
 
 
 if __name__ == '__main__':
@@ -33,5 +34,5 @@ if __name__ == '__main__':
     output = re.sub(r'\.zarr$', '_normalized.zarr', args.input)
 
     # copy input zarr file to output zarr file
-    os.system(f'cp {args.input} {output}')
+    os.system(f'cp -r {args.input} {output}')
     apply_normalization(output)
