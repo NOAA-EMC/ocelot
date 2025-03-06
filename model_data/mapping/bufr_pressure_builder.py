abdffad93132868a91008a5131d639a21b987245
Ron McLaren <ronlaren@aol.com>
2025-03-06 10:53:53 -0500

    fixed pressure builder

--- model_data/mapping/bufr_pressure_builder.py
+++ model_data/mapping/bufr_pressure_builder.py
@@ -10,55 +10,46 @@ ADPUPA_MAPPING = './bufr_adpupa_mapping.yaml'
 ADPSFC_MAPPING = './bufr_adpsfc_mapping.yaml'
 SFCSHP_MAPPING = './bufr_sfcshp_mapping.yaml'
 
+OBS_TYPES = np.array([180, 181, 183, 187, 120])
 
-def mask_container(container, mask):
-    new_container = bufr.DataContainer()
-    for field_name in container.list():
-        new_container.add(field_name, container.get(field_name)[mask])
-    return new_container
 
 class PressureObsBuilder(ObsBuilder):
     def __init__(self, input_path):
 
-        input_dict == {'adpups': (input_path, ADPUPA_MAPPING),
+        input_dict == {'adpupa': (input_path, ADPUPA_MAPPING),
                        'adpsfc': (input_path, ADPSFC_MAPPING),
                        'sfcshp': (input_path, SFCSHP_MAPPING)}
 
         super().__init__(input_dict, log_name=os.path.basename(__file__))
 
-    # Virtual Method
-    def _make_description(self) -> bufr.encoders.Description:
-        description = super()._make_description()
-
-        description.add_variables([
-            {
-                'name': 'virtualTemperature',
-                'source': 'virtualTemperature',
-                'units': 'K',
-                'longName': 'Virtual Temperature'
-            }
-        ])
-
-        return description
-
 
     # Virtual Method
     def _make_obs(self, comm) -> bufr.DataContainer:
-        container = super()._make_obs(comm)
+        adpupa_container = bufr.Parser(*self.input_dict['adpupa']).parse(comm)
+        adpsfc_container = bufr.Parser(*self.input_dict['adpsfc']).parse(comm)
+        sfcshp_container = bufr.Parser(*self.input_dict['sfcshp']).parse(comm)
+
+        # Mask ADPUPA for station pressure category
+        data_level_cat = adpupa_container.get('dataLevelCategory')
+        adpupa_container.apply_mask(data_level_cat == 0)
+
+        # Remove extra data from ADPUPA (make all containers the same)
+        adpupa_container.remove('dataLevelCategory')
+
+        # Merge containers
+        container = bufr.DataContainer()
+        container.append(adpupa_container)
+        container.append(adpsfc_container)
+        container.append(sfcshp_container)
+
+        # Filter according to thw obs type
+        obs_type = container.get('observationType')
+        container.apply_mask(np.isin(obs_type, OBS_TYPES))
 
         # Wrap the longitude values
         longitude = container.get('longitude')
         longitude = np.where(longitude > 180, longitude - 360, longitude)
         container.replace('longitude', longitude)
-
-        # Filter by type
-        obsType = container.get('observationType')
-        container = mask_container(container, obsType < 200)
-
-        # Add the virtual temperature
-        # temperature = container.get('temperature')
-        # virtual_temperature = []
-        # container.add('virtualTemperature', virtual_temperature)
 
         return container
 
