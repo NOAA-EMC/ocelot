708a1706c3828513b594b174bb881b7467539029
Ron McLaren <ronlaren@aol.com>
2025-03-28 10:11:53 -0400

    added gen_data script

---
+++ data_preperation/scripts/gen_data.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import argparse
+from datetime import datetime
+
+base_path = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.realpath(os.path.join(base_path, '../src')))
+
+from gen_model_data import create_data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start_date')
+    parser.add_argument('end_date')
+    parser.add_argument('type')
+
+    args = parser.parse_args()
+
+    start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
+
+    create_data(start_date, end_date, args.type)
+
