6e76bd7879471feb0abd9a199a8d05ac59c0a346
Azadeh Gholoubi <Azadeh.gholoubi@gmail.com>
2025-06-29 18:28:51 +0000

    added error plot

--- gnn_model/evaluations.py
+++ gnn_model/evaluations.py
@@ -2,17 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load your predictions CSV
-df = pd.read_csv("step11_fineTunning_month2/bt_predictions_epoch10.csv")
+df = pd.read_csv("bt_predictions_epoch9.csv")
 
 # Plot predictions vs targets for each channel
 for i in range(1, 23):  # Channels 1 to 22
-    plt.figure(figsize=(12, 5))
+    plt.figure(figsize=(16, 5))
     # Get shared color scale limits
     vmin = min(df[f"true_bt_{i}"].min(), df[f"pred_bt_{i}"].min())
     vmax = max(df[f"true_bt_{i}"].max(), df[f"pred_bt_{i}"].max())
 
     # Plot target
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     sc1 = plt.scatter(df["lon_deg"], df["lat_deg"], c=df[f"true_bt_{i}"], cmap="viridis", s=5, vmin=vmin, vmax=vmax)
     plt.colorbar(sc1)
     plt.title(f"True Brightness Temp - Channel {i}")
@@ -20,13 +20,21 @@ for i in range(1, 23):  # Channels 1 to 22
     plt.ylabel("Latitude")
 
     # Plot prediction
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     sc2 = plt.scatter(df["lon_deg"], df["lat_deg"], c=df[f"pred_bt_{i}"], cmap="viridis", s=5, vmin=vmin, vmax=vmax)
     plt.colorbar(sc2)
     plt.title(f"Predicted Brightness Temp - Channel {i}")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
 
+    plt.subplot(1, 3, 3)
+    sc2 = plt.scatter(df["lon_deg"], df["lat_deg"], c=((df[f"true_bt_{i}"]-df[f"pred_bt_{i}"])/df[f"true_bt_{i}"])*100, cmap="bwr", s=5)
+    plt.colorbar(sc2)
+    plt.title(f"Error Brightness Temp - Channel {i}")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+
     plt.tight_layout()
-    plt.savefig(f"step11_fineTunning_month2/bt_map_channel_{i}.png")
+    plt.savefig(f"bt_map_channel_{i}.png")
     plt.close()
+    print(f"finished ch_{i}")
