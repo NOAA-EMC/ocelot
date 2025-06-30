83f935f85a2311bb9da84a1e6ee576d770dddf0a
Azadeh Gholoubi <Azadeh.gholoubi@gmail.com>
2025-06-30 00:54:10 +0000

    updated error plots

--- gnn_model/evaluations.py
+++ gnn_model/evaluations.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import TwoSlopeNorm
 
 # Load your predictions CSV
-df_bt = pd.read_csv("bt_predictions_epoch14.csv")
-df_pressure = pd.read_csv("pressure_predictions_epoch14.csv")
+df_bt = pd.read_csv("bt_predictions_epoch9.csv")
+df_pressure = pd.read_csv("pressure_predictions_epoch9.csv")
 
 plt.figure(figsize=(12, 5))
 # Get shared color scale limits
@@ -28,17 +29,18 @@ plt.title(f"Predicted Pressure")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 
+error = ((df_pressure["true_pressure"]-df_pressure["pred_pressure"])/df_pressure["true_pressure"])*100
+norm = TwoSlopeNorm(vmin=error.min(), vcenter=0, vmax=error.max())
 plt.subplot(1, 3, 3)
-sc2 = plt.scatter(
-    df_pressure["lon_deg"], df_pressure["lat_deg"], c=((df_pressure["true_pressure"]-df_pressure["pred_pressure"])/df_pressure["true_pressure"])*100,
-    cmap="bwr", s=5)
-plt.colorbar(sc2)
+sc3 = plt.scatter(df_pressure["lon_deg"], df_pressure["lat_deg"], c=error, norm=norm, cmap="bwr", s=5)
+plt.colorbar(sc3)
 plt.title(f"Error Pressure")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.tight_layout()
 plt.savefig(f"pressure_map.png")
 plt.close()
+print(f"finished pressure_map.png")
 
 # Plot predictions vs targets for each channel
 for i in range(1, 23):  # Channels 1 to 22
@@ -63,14 +65,16 @@ for i in range(1, 23):  # Channels 1 to 22
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
 
+    error = ((df_bt[f"true_bt_{i}"] - df_bt[f"pred_bt_{i}"]) / df_bt[f"true_bt_{i}"]) * 100
+    norm = TwoSlopeNorm(vmin=error.min(), vcenter=0, vmax=error.max())
     plt.subplot(1, 3, 3)
-    sc2 = plt.scatter(df["lon_deg"], df["lat_deg"], c=((df[f"true_bt_{i}"]-df[f"pred_bt_{i}"])/df[f"true_bt_{i}"])*100, cmap="bwr", s=5)
-    plt.colorbar(sc2)
-    plt.title(f"Error Brightness Temp - Channel {i}")
+    sc3 = plt.scatter(df_bt["lon_deg"], df_bt["lat_deg"], c=error, cmap="bwr", norm=norm, s=5)
+    plt.colorbar(sc3)
+    plt.title(f"Error % Brightness Temp - Channel {i}")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
 
     plt.tight_layout()
     plt.savefig(f"bt_map_channel_{i}.png")
     plt.close()
-    print(f"finished ch_{i}")
+    print(f"finished ch_{i}.png")
