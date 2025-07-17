6b446b44a7043d0dfee51070b26e30c2ba571e14
Ron McLaren <ronald.mclaren@noaa.gov>
2025-07-16 22:30:18 -0400

    back to using custom actions

--- aws/configuration/on_node_start.sh
+++ aws/configuration/on_node_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt update -y
+#sudo apt update -y
 
-# Install Python 3./10
+# Install Python 3.10
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
+#sudo apt-get update
 sudo apt install -y python3.10 python3.10-distutils python3.10-venv
