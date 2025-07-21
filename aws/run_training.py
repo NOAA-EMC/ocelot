c569cd9366097453825caa6e4c3b101307f7afe5
Ron McLaren <ronald.mclaren@noaa.gov>
2025-07-21 13:51:31 -0400

    working path twaek

--- aws/run_training.py
+++ aws/run_training.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('--cluster-name', default='ocelot-training', help='Name of the cluster')
     parser.add_argument('--branch', default='main', help='Ocelot branch to use')
     parser.add_argument('--instance-type', default='g5.2xlarge', help='EC2 instance type for compute nodes')
-    parser.add_argument('--script', default='gnn_model/train_gnn.py', help='Python script to run on the cluster')
+    parser.add_argument('--script', default='train_gnn.py', help='Python script to run on the cluster')
     parser.add_argument('--num_nodes', type=int, help='Number of nodes in the cluster.')
     parser.add_argument('--cpus_per_task', type=int, default=4, help='Number of CPUs per node')
     parser.add_argument('--keep-cluster', action='store_true', help='Do not delete the cluster after completion')
@@ -35,7 +35,7 @@ def main():
     cluster = Cluster(args.cluster_name, args.instance_type, args.num_nodes, args.cpus_per_task)
     init_ocelot_branch(cluster, args.branch)
 
-    cluster.head_node.srun(args.script, num_nodes=args.num_nodes, cpus_per_task=args.cpus_per_task, path="ocelot")
+    cluster.head_node.srun(args.script, num_nodes=args.num_nodes, cpus_per_task=args.cpus_per_task, path="ocelot/gnn_model")
 
     if not args.keep_cluster:
         cluster.delete()
