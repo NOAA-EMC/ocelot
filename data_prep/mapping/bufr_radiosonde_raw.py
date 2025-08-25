843ea86401a3a2d61c85930859d2f48126c7e484
Ron McLaren <ronald.mclaren@noaa.gov>
2025-08-25 10:36:09 -0400

    radiosonde enhancemenst

--- data_prep/mapping/bufr_radiosonde_raw.py
+++ data_prep/mapping/bufr_radiosonde_raw.py
@@ -32,7 +32,15 @@ class RawRadiosondeBuilder(ObsBuilder):
 
         # Add timestamps to the prepbufr container
         reference_time = self._get_reference_time(input_dict[PrepbufrKey])
-        self._add_timestamp(prepbufr_container, reference_time)
+        self._add_timestamp('launchCycleTime',
+                            'launchTime',
+                            prepbufr_container,
+                            reference_time)
+
+        self._add_timestamp('driftCycleTime',
+                            'driftTime',
+                            prepbufr_container,
+                            reference_time)
 
         # Add the prepbufr quality flag fields to the combined container
         # Use the timestamp, latitude and longitude to match the observations
@@ -77,4 +85,98 @@ class RawRadiosondeBuilder(ObsBuilder):
             container.add(var, quality_flags, ['*'])
 
         return container
+
+    def _make_description(self):
+        description = bufr.encoders.Description(self.map_dict[DumpKey])
+
+        # Add the quality flag variables
+        description.add_variables([
+            {
+                'name': "time",
+                'source': 'driftTime',
+                'longName': "Datetime",
+                'units': "seconds since 1970-01-01T00:00:00Z"
+            },
+            {
+                'name': "latitude",
+                'source': 'driftLatitude',
+                'longName': "Latitude",
+                'units': "degree_north"
+            },
+            {
+                'name': "longitude",
+                'source': 'driftLongitude',
+                'longName': "Longitude",
+                'units': "degree_east"
+            },
+            {
+                'name': "height",
+                'source': 'height',
+                'longName': "Height",
+                'units': "meters"
+            },
+            {
+                'name': "airTemperatureQuality",
+                'source': 'airTemperatureQuality',
+                'longName': "Air Temperature Quality Marker",
+                'units': "quality_marker"
+            },
+            {
+                'name': "specificHumidityQuality",
+                'source': 'specificHumidityQuality',
+                'longName': "Specific Humidity Quality Marker",
+                'units': "quality_marker"
+            },
+            {
+                'name': "dewPointTemperatureQuality",
+                'source': 'dewPointTemperatureQuality',
+                'longName': "Dew Point Temperature Quality Marker",
+                'units': "quality_marker"
+            },
+            {
+                'name': "windQuality",
+                'source': 'windQuality',
+                'longName': "Wind Quality Marker",
+                'units': "quality_marker"
+            },
+            {
+                'name': "airPressureQuality",
+                'source': 'airPressureQuality',
+                'longName': "Air Pressure Quality Marker",
+                'units': "quality_marker"
+            },
+            {
+                'name': "heightQuality",
+                'source': 'heightQuality',
+                'longName': "Height Quality Marker",
+                'units': "quality_marker"
+            }
+        ])
+
+        description.add_dimension('event', ['*', '*/EVENT'])
+
+        return description
+
+    def _get_reference_time(self, input_path) -> np.datetime64:
+        path_components = Path(input_path).parts
+        m = re.match(r'\w+\.(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})', path_components[-4])
+
+        if not m.groups():
+            raise Exception("Error: Path string did not match the expected pattern.")
+
+        return np.datetime64(datetime(year=int(m.group('year')),
+                                      month=int(m.group('month')),
+                                      day=int(m.group('day')),
+                                      hour=int(path_components[-3])))
+
+    def _add_timestamp(self,
+                       input_name: str,
+                       output_name: str,
+                       container: bufr.DataContainer,
+                       reference_time: np.datetime64) -> None:
+        cycle_times = np.array([3600 * t for t in container.get(input_name)]).astype('timedelta64[s]')
+        time = (reference_time + cycle_times).astype('datetime64[s]').astype('int64')
+        container.add(output_name, time, ['*'])
+
+add_main_functions(RawRadiosondeBuilder)
 
