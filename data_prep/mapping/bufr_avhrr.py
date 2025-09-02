07974a4baeff46415f2c3ee5510d82830aa85dda
Ron McLaren <ronald.mclaren@noaa.gov>
2025-09-02 14:57:52 -0400

    extended avhrr to make more complete exaple

--- data_prep/mapping/bufr_avhrr.py
+++ data_prep/mapping/bufr_avhrr.py
@@ -3,7 +3,7 @@
 import os
 
 import bufr
-from bufr.obs_builder import ObsBuilder, add_main_functions, map_path
+from bufr.obs_builder import ObsBuilder, add_main_functions, map_path, FILE_ENCODER_DICT
 
 MAPPING_PATH = map_path('bufr_avhrr.yaml')
 
@@ -24,6 +24,57 @@ class AvhrrObsBuilder(ObsBuilder):
             container.append(bufr.Parser(input_dict[PM_KEY], self.map_dict[PM_KEY]).parse(comm))
 
         return container
+
+    def create_obs_file(self, am_input, pm_input, output, type='netcdf', append=False):
+        """
+        Create an observation file from the input data. Override this method if you want to
+        customize the file creation process or if you need a different function signature (ex: you
+        need to pass multiple input files). add_main_functions will copy the function signature.
+
+        :param am_input: Input path to the BUFR file.
+        :param pm_input: Input path to the BUFR file.
+        :param output: Output file name
+        :param type: Data type to encode into (optional)
+        :param append: Add to the file if it exists or create a new file. (optional)
+        """
+
+        comm = bufr.mpi.Comm("world")
+        self.log.comm = comm
+
+        container = self.make_obs(comm, {AM_KEY:am_input, PM_KEY:pm_input})
+        container.gather(comm)
+
+        # Encode the data
+        if comm.rank() == 0:
+            self.finalize_container(container)
+            FILE_ENCODER_DICT[type](self.description).encode(container, output, append)
+
+        self.log.info(f'Return the encoded data')
+
+    def create_obs_group(self, am_input, pm_input, env):
+        """
+        Create an observation file from the input data. Override this method if you want to
+        customize the file creation process or if you need a different function signature (ex: you
+        need to pass multiple input files).
+
+        :param input: Input path to the BUFR file.
+        :param env: The IODA environment. Dictionary with keys: start_time, end_time, comm_name
+        :return: IODA ObsGroup object.
+        """
+        from pyioda.ioda.Engines.Bufr import Encoder as iodaEncoder
+
+        comm = bufr.mpi.Comm(env["comm_name"])
+        self.log.comm = comm
+
+        container = self.make_obs(comm, {AM_KEY:am_input, PM_KEY:pm_input})
+        container.all_gather(comm)
+
+        self.finalize_container(container)
+
+        self.log.info(f'Encoding')
+        data = next(iter(iodaEncoder(self.description).encode(container).values()))
+
+        return data
 
     def _make_description(self):
         description = bufr.encoders.Description(self.map_dict[AM_KEY])
