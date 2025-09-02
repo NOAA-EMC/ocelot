6de878512c7454c3ed7068161b0b4f60e58fee69
Ron McLaren <ronald.mclaren@noaa.gov>
2025-09-02 14:28:16 -0400

    updated avhrr code

--- data_prep/mapping/bufr_avhrr.py
+++ data_prep/mapping/bufr_avhrr.py
@@ -7,9 +7,23 @@ from bufr.obs_builder import ObsBuilder, add_main_functions, map_path
 
 MAPPING_PATH = map_path('bufr_avhrr.yaml')
 
+AM_KEY = 'am'
+PM_KEY = 'pm'
+
 class AvhrrObsBuilder(ObsBuilder):
     def __init__(self):
-        super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
+        super().__init__({AM_KEY:MAPPING_PATH,
+                          PM_KEY:MAPPING_PATH}, log_name=os.path.basename(__file__))
+
+    def make_obs(self, comm, input_dict):
+        container = bufr.Container()
+        if os.path.exists(input_dict[AM_KEY]):
+            container.append(bufr.Parser(input_dict[AM_KEY], self.map_dict[AM_KEY]).parse(comm))
+
+        if os.path.exists(input_dict[PM_KEY]):
+            container.append(bufr.Parser(input_dict[PM_KEY], self.map_dict[PM_KEY]).parse(comm))
+
+        return container
 
 
 # Add main functions create_obs_file and create_obs_group
