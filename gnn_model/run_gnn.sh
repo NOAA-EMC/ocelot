266a48d5ed07588244164ced50436cde9819a511
Mu-Chieh Ko <Mu-Chieh.Ko@noaa.gov>
2025-09-23 18:23:52 +0000

    reversing style 'fixing' on run_gnn.sh

--- gnn_model/run_gnn.sh
+++ gnn_model/run_gnn.sh
@@ -16,32 +16,32 @@
 
 
 # Load Conda environment
-source / scratch3/NCEPDEV/da/Azadeh.Gholoubi/miniconda3/etc/profile.d/conda.sh
+source /scratch3/NCEPDEV/da/Azadeh.Gholoubi/miniconda3/etc/profile.d/conda.sh
 conda activate gnn-env
 
 # PYTHONPATH
 # export PYTHONPATH=/scratch3/NCEPDEV/da/Azadeh.Gholoubi/tmp/lib/python3.10/site-packages:$PYTHONPATH
 
 # Debug + performance
-export NCCL_DEBUG = INFO
-export NCCL_DEBUG_SUBSYS = INIT, NET
-export TORCH_NCCL_BLOCKING_WAIT = 1          # explicit
-export NCCL_SHM_DISABLE = 1                  # avoid shm edge cases
-export NCCL_NET_GDR_LEVEL = PHB              # conservative GPUDirect setting
-export NCCL_IB_DISABLE = 0
-export OMP_NUM_THREADS = 1
-export PYTORCH_ENABLE_MPS_FALLBACK = 1
-export NCCL_SOCKET_IFNAME = ib0
-export NCCL_IB_DISABLE = 0
-export TORCH_NCCL_ASYNC_ERROR_HANDLING = 1
-export NCCL_P2P_LEVEL = NVL
-export PYTHONFAULTHANDLER = 1
-export TORCH_DISTRIBUTED_DEBUG = OFF  # INFO
+export NCCL_DEBUG=INFO
+export NCCL_DEBUG_SUBSYS=INIT,NET
+export TORCH_NCCL_BLOCKING_WAIT=1          # explicit
+export NCCL_SHM_DISABLE=1                  # avoid shm edge cases
+export NCCL_NET_GDR_LEVEL=PHB              # conservative GPUDirect setting
+export NCCL_IB_DISABLE=0
+export OMP_NUM_THREADS=1
+export PYTORCH_ENABLE_MPS_FALLBACK=1
+export NCCL_SOCKET_IFNAME=ib0
+export NCCL_IB_DISABLE=0
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_P2P_LEVEL=NVL
+export PYTHONFAULTHANDLER=1
+export TORCH_DISTRIBUTED_DEBUG=OFF # INFO
 # export CUDA_LAUNCH_BLOCKING=1
-export PYTORCH_CUDA_ALLOC_CONF = expandable_segments: True
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # Local NNJA mirror (shared path visible to ALL nodes)
-export NNJA_LOCAL_ROOT = /scratch3/NCEPDEV/da/Azadeh.Gholoubi/NNJA/nnja-ai
-export PYTHONPATH = /scratch3/NCEPDEV/da/Azadeh.Gholoubi/NNJA/ocelot/gnn_model: / scratch3/NCEPDEV/da/Azadeh.Gholoubi/NNJA/ocelot: $PYTHONPATH
+export NNJA_LOCAL_ROOT=/scratch3/NCEPDEV/da/Azadeh.Gholoubi/NNJA/nnja-ai
+export PYTHONPATH=/scratch3/NCEPDEV/da/Azadeh.Gholoubi/NNJA/ocelot/gnn_model:/scratch3/NCEPDEV/da/Azadeh.Gholoubi/NNJA/ocelot:$PYTHONPATH
 
 echo "Running on $(hostname)"
 echo "SLURM Node List: $SLURM_NODELIST"
@@ -49,7 +49,7 @@ echo "Visible GPUs on this node:"
 nvidia-smi
 
 # Launch training (env is propagated to ranks)
-srun - -export = ALL - -kill-on-bad-exit = 1 - -cpu-bind = cores python train_gnn.py
+srun --export=ALL --kill-on-bad-exit=1 --cpu-bind=cores python train_gnn.py
 
 # Resume training from the latest checkpoint
 # srun --export=ALL --kill-on-bad-exit=1 --cpu-bind=cores python train_gnn.py --resume_from_latest
