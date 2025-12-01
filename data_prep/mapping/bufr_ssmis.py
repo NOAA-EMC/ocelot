7217dbab5826289162f60de4f4204527accb2e07
Ron McLaren <ronald.mclaren@noaa.gov>
2025-12-01 11:21:21 -0500

    filtering for satellite f17

--- data_prep/mapping/bufr_ssmis.py
+++ data_prep/mapping/bufr_ssmis.py
@@ -10,6 +10,15 @@ class BufrSsmisObsBuilder(ObsBuilder):
     def __init__(self):
         super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
 
+    def make_obs(self, comm, input_path):
+        container = super().make_obs(comm, input_path)
+
+        # Mask out values with non-zero quality flags
+        sat_id = container.get('satalliteIdentifier')
+        container.apply_mask(sat_id == 285)
+
+        return container
+
 
 # Add main functions create_obs_file or create_obs_group
 add_main_functions(BufrSsmisObsBuilder)
